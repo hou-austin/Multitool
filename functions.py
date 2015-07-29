44875706fa4d45770312193293311e87ceef1ae7
Dark Leviathan <you@example.com>
2015-07-29 12:06:53 -0700

    Added 2 new functions with bug fixes
    Functions added:
    - Time
    - Weather
    
    Bug fixed:
    - Calculator
        - “ans” can now pass scan while “sqrt()” will not without any
    numbers

--- functions.py
+++ functions.py
@@ -2,6 +2,8 @@ import os, sys, glob, math, random, login, gfx, config, time, urllib.request, re
 from math import sqrt
 from lxml import html
 from urllib.request import urlopen, HTTPError
+from time import gmtime, strftime
+from pprint import pprint
 re.compile('<title>(.*)</title>')
 
 def load_sequence_complete():
@@ -57,8 +59,15 @@ def mainframe(user_input):
     elif (user_input == "help"):
         print ("0")
         choice_help()
-    elif (split_line_test(user_input) == True):
+    elif (user_define_input == "define"):
         define_word(user_define_input)
+    elif (user_input == "time"):
+        show_time()
+    elif (split_line_test(user_input)):
+        if (user_define_input[0] == "define"):
+            define_word(user_define_input)
+        if (user_define_input[0] == "weather"):
+            show_weather(user_define_input)
     else:
         calculator(user_input)
 
@@ -73,9 +82,9 @@ def calculator(user_input):
     if any(c not in config.valid_cal_chars for c in user_input):
         print("- Invalid Equation | Bad characters")
         return
-    """elif not any(c in user_input for c in "0123456789"):
+    elif not any("ans" in user_input or c in user_input for c in "0123456789"):
         print("- Invalid Equation | No numbers found")
-        return"""
+        return
     sys.stdout.write("calculating " + "-".join(gfx.load_sequence))
     time.sleep(0.3)
     print (" | 100%")
@@ -110,7 +119,7 @@ def define_word(user_define_input):
     title = tree.xpath('//title/text()')
     print(title)
     print("\n")
-    defs = tree.xpath('//span[@class="def-content"]/text()') # print(defs)
+    defs = tree.xpath('//div[@class="def-content"]/text()')
     defs = '\n'.join(defs)
     defs = defs.split('\n')
     defs = [d for d in defs if d]
@@ -122,4 +131,33 @@ def split_line_test(user_input):
     user_define_input = user_input.split()
     if (user_define_input[0] == "define"):
         return True
+    if (user_define_input[0] == "weather"):
+        return True
     return False
+
+def show_time():
+    print(time.ctime())# 'Mon Oct 18 13:35:29 2010'
+    print(time.strftime('%l:%M%p %Z on %b %d, %Y')) # ' 1:36PM EDT on Oct 18, 2010'
+    print(time.strftime('%l:%M%p %z on %b %d, %Y')) # ' 1:36PM EST on Oct 18, 2010'
+
+def show_weather(user_define_input):
+    print ("Retrieving Weather data | " + "-".join(gfx.load_sequence))
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + (user_define_input[1]))
+    requested_data = r.json()
+    print ("Data retrieved \n")
+    kelvin_norm_temp = requested_data ["main"]["temp"]
+    kelvin_max_temp = requested_data ["main"]["temp_max"]
+    kelvin_min_temp = requested_data ["main"]["temp_min"]
+    celcius_norm_temp = str(kelvin_celcius(kelvin_norm_temp))
+    celcius_max_temp = str(kelvin_celcius(kelvin_max_temp))
+    celcius_min_temp = str(kelvin_celcius(kelvin_min_temp))
+    print ("Normal temperature is: %.1f" % (celcius_norm_temp))
+    print ("Highest temperature is: %.1f" % (celcius_max_temp))
+    print ("Lowest temperature is: %.1f" % (celcius_min_temp))
+    print ("\n")
+
+def kelvin_celcius(temp):
+    kelvin = float(273.15)
+    temp = float(temp)
+    output_temp = (temp - kelvin)
+    return output_temp
